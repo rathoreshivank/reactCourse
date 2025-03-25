42903815495f43c7c5f42dc13003666e5f68d86e
Shivank Singh <shivankrathore.31@gmail.com>
2025-03-25 11:25:20 +0530

    made a todo App with local storage used contextApi

--- 08miniContext/src/context/UserContextprovide.jsx
+++ 08miniContext/src/context/UserContextprovide.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import UserContext from "./UserContext";
 
 export const UserContextprovider = ({children}) => {
+    // Providing `user` and `setUser` to all children components
     const [user, setUser] = React.useState(null)
     return (
         <UserContext.Provider value={{user, setUser}}> 
