94e534f2a25fc10270a7ab9c2fa05b1296c5315c
Shivank Singh <shivankrathore.31@gmail.com>
2025-04-24 20:23:48 +0530

    minor changes

--- theme-switcher/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ThemeProvider } from './context/ThemeContext.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-)
